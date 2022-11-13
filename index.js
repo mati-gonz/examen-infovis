c6272cc1b16723885b38a0b1a5ccdf8b0763e8db
MATIAS GONZALEZ <matigonz@uc.cl>
2022-11-13 19:12:51 -0300

    new data

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
+// Constantes y funciones de procesamiento de datos
+
 const WIDTH_VIS1 = 800;
 const HEIGHT_VIS1 = 600;
 const WIDTH_VIS2 = 1000;
 const HEIGHT_VIS2 = 1000;
 const DATA = "https://raw.githubusercontent.com/mati-gonz/examen-infovis/main/assets/data/CR7_DATA.csv";
+const mesesTemporadaRegular = ["Aug", "Sep", "Oct", "Nov", "Dec", "Jan", "Feb", "Mar", "Apr", "May"];
 
 var parseTime = d3.timeParse("%m/%d/%Y")
 
@@ -15,6 +18,8 @@ var parseData= function(d) {
     };
 }
 
+// Construccion de las visualizaciones
+
 const svg1 = d3.select("#container-vis-1")
     .append("svg")
     .attr("width", WIDTH_VIS1)
@@ -26,6 +31,9 @@ const svg2 = d3.select("#container-vis-2")
     .attr("width", WIDTH_VIS2)
     .attr("height", HEIGHT_VIS2)
     .attr("class", "vis-2");
+
+
+// Carga de datos
 
 const datos = d3.csv(DATA, parseData)
     .then(data => {
