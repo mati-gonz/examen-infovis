d16b77d06e0d3775b3ad9cf7c5903d4b61529811
MATIAS GONZALEZ <matigonz@uc.cl>
2022-11-21 00:40:07 -0300

    avance areas

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // Constantes y funciones de procesamiento de datos
 
 MARGIN_1 = {
-    top: 30,
+    top: 50,
     bottom: 30,
-    right: 35,
-    left: 35,
+    right: 50,
+    left: 100,
 }
 
 MARGIN_2 = {
@@ -14,8 +14,8 @@ MARGIN_2 = {
     left: 100,
 }
 
-const WIDTH_1 = 800;
-const HEIGHT_1 = 700;
+const WIDTH_1 = 1200;
+const HEIGHT_1 = 800;
 const WIDTH_2 = 750;
 const HEIGHT_2 = 700;
 
@@ -29,10 +29,26 @@ const logoHeight = 110;
 const logoWidth = 60;
 
 const DATA = "https://raw.githubusercontent.com/mati-gonz/examen-infovis/main/assets/data/CR7_DATA.csv";
-const mesesTemporadaRegular = ["Aug", "Sep", "Oct", "Nov", "Dec", "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul"];
-const colores = ["green", "red", "blue", "black"]
 
-var parseTime = d3.timeParse("%m/%d/%Y")
+const mesesTemporadaRegular = {
+    12: "Ene",
+    13: "Feb",
+    14: "Mar",
+    15: "Abr",
+    16: "May",
+    17: "Jun",
+    18: "Jul",
+    19: "Ago",
+    7: "Ago",
+    8: "Sep",
+    9: "Oct",
+    10: "Nov",
+    11: "Dic"
+};
+
+const colores = ["green", "red", "blue", "black"];
+
+var parseTime = d3.timeParse("%m/%d/%Y");
 
 var parseData= function(d) {
     return {
@@ -61,29 +77,118 @@ const svg2 = d3.select("#container-vis-2")
 const contenedorEjeX2 = svg2
     .append("g")
     .attr("transform", `translate(${MARGIN_2.left}, ${HEIGHT_VIS2})`)
+    
+const contenedorEjeX = svg1
+    .append("g")
+    .attr("transform", `translate(${MARGIN_1.left}, ${HEIGHT_VIS1})`)
+
+const contenedorEjeY = svg1
+    .append("g")
+    .attr("transform", `translate(${MARGIN_1.left}, 0)`)
 
 
 function joinData(data) {
     const grupoSeason = d3.groups(data, d => d.season)
     const grupoEquipo = d3.groups(data, d => d.club)
 
+    grupoSeason.map((d) => {
+        var nuevo_año = false;
+        d[1].map( fecha => {
+            if (fecha.date.getMonth() < 7){
+                nuevo_año = true;
+            }
+            if (nuevo_año){
+                fecha.date = fecha.date.getMonth() + 12;
+            } else {
+                fecha.date = fecha.date.getMonth();
+            }
+        })
+    });
+
+    const agruparGolesMes = d3.groups(grupoSeason, (d) => {
+        const grupoFechas = d3.groups(d[1], b => b.date)
+        return grupoFechas
+    })
+
+    const COLOR = d3.scaleOrdinal(colores).domain(data.map(d => d.club))
+
+    console.log(agruparGolesMes)
+
+
+    //console.log(grupoSeason)
+
     // VISUALIZACIÓN 1
 
-    console.log(grupoSeason);
+    // EJE X
 
-    // const escalaX = d3
-    //     .scalePoint()
-    //     .domain(d3.range(mesesTemporadaRegular.length))
-    //     .range([0, WIDTH_VIS1])
-    //     .padding(1);
+    const mesMinimo = d3.min(grupoSeason, d => d3.min(d[1], d => d.date))
+    const mesMaximo = d3.max(grupoSeason, d => d3.max(d[1], d => d.date))
+
+    const escalaX = d3
+        .scaleLinear()
+        .domain([mesMinimo, mesMaximo])
+        .range([0, WIDTH_VIS1])
     
-    // const escalaY = d3
-    //     .scaleLinear()
-    //     .domain([
-    //       0,
-    //       d3.max(grupoSeason, (serie) => d3.max(serie, (arreglo) => arreglo[1])),
-    //     ])
-    //     .range([HEIGHT - 10, 10]);
+    const ejeX = d3.axisBottom(escalaX);
+    ejeX.tickFormat((d) => mesesTemporadaRegular[d]);
+
+    contenedorEjeX
+        .call(ejeX)
+        .selectAll("text")
+        .attr("font-size", 17)
+    
+    // EJE Y
+
+    const golesMinimoEnUnMes = d3.min(agruparGolesMes, d => d3.min(d[0], b => b[1].length))
+    const golesMaximoEnUnMes = d3.max(agruparGolesMes, d => d3.max(d[0], b => b[1].length))
+
+    const escalaY = d3
+        .scaleLinear()
+        .domain([0, golesMaximoEnUnMes])
+        .range([HEIGHT_VIS1, MARGIN_1.top]);
+
+    const ejeY = d3.axisLeft(escalaY);
+
+    contenedorEjeY
+        .call(ejeY)
+        .selectAll("text")
+        .attr("font-size", 17)
+    
+    const area = d3
+        .area()
+        .curve(d3.curveMonotoneX)
+        .x((d) => {
+            console.log("primer log")
+            console.log(d)
+            console.log(d[0])
+
+            return escalaX(d[0])
+        })
+        .y0(HEIGHT_VIS1)
+        .y1((d) => {
+            console.log("segundo log")
+            console.log(d)
+            console.log(d[1].length)
+
+            return escalaY(d[1].length)});
+    
+    const menosGoles = [agruparGolesMes[0], agruparGolesMes[1]]
+
+    const enterAndUpdate = svg1
+        .selectAll(".goal-area")
+        .data(menosGoles)
+        .join("path")
+        .attr("class", "goal-area")
+        .attr("fill", d => COLOR(d[0][0][1][0].club))
+        .attr("opacity", 0.5)
+        .attr("d", (d) => {
+            console.log("area log")
+            console.log(d[0])
+            return area(d[0])
+        })
+        .attr("transform", `translate(${MARGIN_1.left}, 0)`)
+        .attr("class", "goal-area")
+
 
     // VISUALIZACIÓN 2
 
@@ -99,8 +204,6 @@ function joinData(data) {
         .scaleLinear()
         .domain([0, d3.max(grupoEquipo, d => d[1].length)])
         .range([0, WIDTH_VIS2])
-    
-    const COLOR = d3.scaleOrdinal(colores).domain(data.map(d => d.club))
 
     const ejeX2 = d3.axisBottom(xScale2);
 
